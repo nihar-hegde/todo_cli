b2a2edc886f2d86693ce6e2a8811c1edc5ac739d
nihar-hegde <niharhegde163@gmail.com>
2024-10-05 20:09:01 +0530

    basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    id: usize,
+    title: String,
+    completed: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TodoList {
+    todos: Vec<Todo>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
